b975dc7ef1538984858772199c1f59421724b204
Noel Campos <noel0010@users.noreply.github.com>
2016-10-29 22:40:05 -0600

    Update and rename pygLatin to pygLatin.py

--- pygLatin.py
+++ pygLatin.py
@@ -8,6 +8,6 @@ new_word = word + first + pyg
 new_word = new_word[1:len(new_word)]
 #if else statements no need of elif
 if len(original) > 0 and original.isalpha():
-    print ("this is your word converted to PygLatin" + new_word)
+    print ("this is your word converted to PygLatin: " + new_word)
 else:
     print 'empty'
